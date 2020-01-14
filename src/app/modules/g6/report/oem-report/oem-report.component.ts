080b3a552e805da6da3fdcbf3747b678b0cd8a2d
aygrhn1991 <411610370@qq.com>
2020-01-14 09:27:26 +0800

    1

--- src/app/modules/g6/report/oem-report/oem-report.component.ts
+++ src/app/modules/g6/report/oem-report/oem-report.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { G6ChartService } from 'src/app/services/g6/g6-chart.service';
+import { G6HttpService } from 'src/app/services/g6/g6-http.service';
+import { UtilService } from 'src/app/services/util.service';
+import { Result2 } from 'src/app/models/result.model';
 
 @Component({
   selector: 'app-oem-report',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oem-report.component.css']
 })
 export class OemReportComponent implements OnInit {
-
-  constructor() { }
+  chart1Option: any;
+  chart2Option: any;
+  chart3Option: any;
+  chart4Option: any;
+  chart5Option: any;
+  chart6Option: any;
+  chart7Option: any;
+  chart8Option: any;
+  constructor(private chartService: G6ChartService,
+    private http: G6HttpService,
+    private util: UtilService) { }
 
   ngOnInit() {
+    this.http.g6Report12('-1', -1,
+      this.util.getDayStart(this.util.getMonthStartDay(new Date())).getTime(),
+      this.util.getDayEnd(this.util.getMonthEndDay(new Date())).getTime()).subscribe((data: Result2) => {
+
+        this.chartService.makeReportChart2(data, data, data);
+      })
   }
 
 }
